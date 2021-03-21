f700af3e65461f566713f10d86029d5a8e5ce9ad
lapptomi <lappetelainen.tomi@gmail.com>
2021-03-21 20:18:33 +0200

    Changed response status to 404 from 400

--- server/src/repository/userRepository.ts
+++ server/src/repository/userRepository.ts
@@ -6,7 +6,6 @@ export const getAll = async (): Promise<Array<string>> => {
     const response: QueryResult = await pool.query('SELECT * FROM Users');
     return response.rows as Array<string>;
   } catch (error) {
-    console.log((error as Error).message);
     throw new Error('Error getting all users from database');
   }
 };
--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get('/', (_req: Request, res: Response) => {
   getAll()
     .then((users) => res.status(200).json(users).end())
-    .catch(() => res.status(400).end());
+    .catch(() => res.status(404).end());
 });
 
 export default router;
