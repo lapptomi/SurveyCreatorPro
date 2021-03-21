18ba0fe53e1a0ef5ec7eb1f8e00a11e5f59db9fb
lapptomi <49559618+lapptomi@users.noreply.github.com>
2021-03-22 00:10:59 +0200

    Changed project structure (#2)
    * Project structure changed

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 import cors from 'cors';
-import userRouter from './src/routes/users';
+import userRouter from './routes/users';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(express.static(`${__dirname}/build`));
+app.use(express.static(`${__dirname}/../build`));
 
 app.use('/api/users', userRouter);
 
