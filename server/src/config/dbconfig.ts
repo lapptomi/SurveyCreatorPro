6141810d8c7e0fb3dcaa2ccf17adbb5d3dbee15d
lapptomi <lappetelainen.tomi@gmail.com>
2021-03-21 19:57:23 +0200

    Fixed database env variable name

--- server/src/config/dbconfig.ts
+++ server/src/config/dbconfig.ts
@@ -1,7 +1,7 @@
 import { Pool } from 'pg';
 
 // [database type]://[username]:[password]@[host]:[port]/[database name]
-const connectionString = process.env.DB_URL as string;
+const connectionString = process.env.DATABASE_URL as string;
 
 export const pool = new Pool({
   connectionString,
