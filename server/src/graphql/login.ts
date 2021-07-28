b4741985a0acde517f94aad08e97ab01d4e91fdf
lapptomi <lappetelainen.tomi@gmail.com>
2021-07-28 04:32:57 +0300

    Fixed Eslint errors

--- server/src/graphql/login.ts
+++ server/src/graphql/login.ts
@@ -2,7 +2,7 @@
 /* eslint-disable arrow-body-style */
 import * as bcrypt from 'bcrypt';
 import * as jwt from 'jsonwebtoken';
-import { IToken } from '../types';
+import { IToken, IUser } from '../types';
 import User from '../models/User';
 
 interface LoginArgs {
@@ -27,7 +27,7 @@ export const typeDef = `
 export const resolvers = {
   Mutation: {
     login: async (_root: unknown, args: LoginArgs): Promise<IToken> => {
-      const user = await User.findOne({ email: args.email });
+      const user = await User.findOne({ email: args.email }) as IUser;
       if (!user) {
         throw new Error('User not found');
       }
